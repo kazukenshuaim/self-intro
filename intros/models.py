925e04422bc7b5609b683e28de6b90b75377bb99
kazukenshuaim <kazu.kenshu.aim@gmail.com>
2026-06-15 16:54:57 +0900

    feat: introモデルを追加

--- intros/models.py
+++ intros/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Intro(models.Model):
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='intros'
+    )
+    name = models.CharField(max_length=100)
+    hobby = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-updated_at']
