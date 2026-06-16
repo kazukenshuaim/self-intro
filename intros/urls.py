7c0db41cdd24ac23d623fc9fcad0627c163f1698
kazukenshuaim <kazu.kenshu.aim@gmail.com>
2026-06-16 11:31:30 +0900

    feat:一覧画面、詳細画面

--- intros/urls.py
+++ intros/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IntroListView
+from .views import IntroListView, IntroDetailView
 
 app_name = 'intros'
 
 urlpatterns = [
     path('', IntroListView.as_view(), name='intro_list'),
+    path('<int:pk>/', IntroDetailView.as_view(), name='intro_detail'),
 ]
--- intros/views.py
+++ intros/views.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Intro
 
 
-class IntroListView(LoginRequiredMixin, TemplateView):
+class IntroListView(LoginRequiredMixin, ListView):
+    model = Intro
     template_name = 'intros/intro_list.html'
+    context_object_name = 'intros'
+
+    def get_queryset(self):
+        return Intro.objects.filter(created_by=self.request.user)
+
+
+class IntroDetailView(LoginRequiredMixin, DetailView):
+    model = Intro
+    template_name = 'intros/intro_detail.html'
+    context_object_name = 'intro'
+
+    def get_queryset(self):
+        return Intro.objects.filter(created_by=self.request.user)
